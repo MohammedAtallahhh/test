0d0963e3da305542afe6d8fdaec407eebd208ca7
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2022-12-08 06:38:30 +0200

    Added animated progress bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+
 function App() {
+  const [value, setValue] = useState(0);
+
+  const seconds = 240;
+
+  const text = () =>
+    `0${Math.floor(value / 60)}:${
+      value % 60 < 10 ? value % 10 : 0 + (value % 60)
+    }`;
+
+  useEffect(() => {
+    const timer = setInterval(() => setValue((value) => value + 1), 1000);
+
+    if (value >= seconds) {
+      clearInterval(timer);
+      setValue(0);
+      return;
+    }
+
+    return () => clearInterval(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log({ value });
   return (
     <>
-      <h1>Freelancing.</h1>
+      <h1>Animated circular bar</h1>
+      <div style={{ width: "100px" }}>
+        <CircularProgressbar value={(value / seconds) * 100} text={text()} />
+      </div>
     </>
   );
 }
